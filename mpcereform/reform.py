0ab97e61450462a489449da1a509d53bbfc2dd94
Michael Falk <35712723+michaelgfalk@users.noreply.github.com>
2019-06-03 14:19:46 +1000

    project set up

---
+++ mpcereform/reform.py
@@ -0,0 +1,9 @@
+"""Command for reshaping existing 'manuscripts' database, and porting it to the new structure"""
+import sys
+
+def main():
+    """Main entry point for the script"""
+    pass
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_namespace_packages
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name = 'mpceDatabaseReform',
+    version = '0.01dev',
+    packages = find_namespace_packages(include = ['mpcereform.*']),
+    author = "Michael Falk",
+    description = "Reforms and repopulates the new MPCE database from the 'manuscripts' version.",
+    license = "MIT",
+    keywords = "MariaDB enlightenment french-book-trade",
+    url="http://fbtee.uws.edu.au/mpce/",
+    entry_points={
+        'console_scripts': ['reform-db=mpcereform.reform:main']
+    }
+)
